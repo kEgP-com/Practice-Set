e60320e1432b8cf1a5a41dfa60f3f1e519be3128
Yasmien Aira Regidor <yas.aira@outlook.com>
2025-10-16 09:48:46 +0800

    changes

--- Yass.php
+++ Yass.php
@@ -2,6 +2,7 @@
 echo <<<QUOTE 
 "Two things are infinite: the universe and human stupidity; 
 and I'm not sure about the universe."
-    - BY ALBERT EINSTEIN
+
+    - by: ALBERT EINSTEIN
 QUOTE;
 ?>
